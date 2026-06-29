b7d3da2a04a05581be0eef6afa6658f52d3c4d01
mloonaa <hey.changjun@gmail.com>
2026-06-29 17:25:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Genichiro /ˌɡɛnɪˈtʃiːroʊ/
---
+++ packages/eslint-config/base.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    eslintConfigPrettier,
+  ],
+  languageOptions: {
+    parserOptions: {
+      /* typed linting */
+      projectService: true,
+    },
+  },
+})
