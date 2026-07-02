6f486ee7839e38fa28d6c94f9bd95c2265771149
mloonaa <hey.changjun@gmail.com>
2026-07-02 15:51:01 +0800

    test ci

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-console.log('test ci 2')
+console.log('test ci cwp', process.cwd())
 export default defineConfig({
   files: ['**/*.{js,ts}'],
   extends: [
